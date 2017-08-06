0004f0ef97acd233d08d520d65ce7f1894966093
jj.owens <jj.owens.dev@gmail.com>
2017-08-06 17:32:23 -0400

    added profile lookup

---
+++ Profile Lookup.js
@@ -0,0 +1,49 @@
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+    var name = contacts.filter(function (object){
+        if(object.firstName === firstName){
+            return object.firstName;
+        }
+    })[0];
+
+    if(!name){
+        return "No such contact";
+    } else {
+        return name[prop] || "No such property";
+    }
+
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
